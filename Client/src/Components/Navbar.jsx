628ac8234482c473ab825c6d8ead2f98426c666b
raja-mohsin1521 <mohsinmunir150@gmail.com>
2024-05-21 23:50:31 +0500

    Some Updates

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -2,30 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAuth } from '../Store/auth';
 
 function Navbar() {
-  const token = localStorage.getItem('Token');
+  const { token } = useAuth();
 
   return (
-    <Navigation>
-      <div className='logo'>
-        <img src="" alt="Logo" />
-      </div>
-      <div className='navLinks'>
-        <NavLink className='link' to='/'>Home</NavLink>
-        <NavLink className='link' to='/shop'>Shop</NavLink>
-        <NavLink className='link' to='/about'>About</NavLink>
-        <NavLink className='link' to='/contact'>Contact Us</NavLink>
-        {token ? (
-          <NavLink className='link' to='/logout'>Logout</NavLink>
-        ) : (
-          <span>
-            <NavLink className='link' to='/login'>Login</NavLink>
-            <NavLink className='link' to='/signup'>Signup</NavLink>
-          </span>
-        )}
-      </div>
-    </Navigation>
+    <>
+      <Navigation>
+        <div className='logo'>
+          <img src="" alt="Logo" />
+        </div>
+        <div className='navLinks'>
+          <NavLink className='link' to='/'>Home</NavLink>
+          <NavLink className='link' to='/shop'>Shop</NavLink>
+          <NavLink className='link' to='/about'>About</NavLink>
+          <NavLink className='link' to='/contact'>Contact Us</NavLink>
+          {token ? (
+            <NavLink className='link' to='/logout'>Logout</NavLink>
+          ) : (
+            <span>
+              <NavLink className='link' to='/login'>Login</NavLink>
+              <NavLink className='link' to='/signup'>Signup</NavLink>
+            </span>
+          )}
+        </div>
+      </Navigation>
+    </>
   );
 }
 
